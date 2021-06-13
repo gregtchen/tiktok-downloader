706560e9c7d667722c11fd2f025a6c67e8b4ca3e
gregtchen <gregory.chen.125@my.csun.edu>
2021-06-13 03:29:22 -0700

    added script

---
+++ download.py
@@ -0,0 +1,16 @@
+import random
+from TikTokApi import TikTokApi
+api = TikTokApi.get_instance()
+
+VERIFY_FP = ""
+did = str(random.randint(10000, 999999999))
+
+results = 10
+
+trending = api.trending(count=results, custom_verifyFp=VERIFY_FP, custom_did = did)
+
+for tiktok in trending:
+    print(tiktok['id'])
+    videobuffer = api.get_video_by_tiktok(tiktok, custom_did=did)
+    with open(tiktok['id'] + ".mp4", "wb") as out:
+        out.write(videobuffer)
